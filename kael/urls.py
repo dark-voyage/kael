3890743d6c2d52f8030f31f45318d16c012eb81b
Sokhibjon Orzikulov <sakhib.orzklv@icloud.com>
2021-02-05 04:45:33 +0500

    changing all things

--- kael/urls.py
+++ kael/urls.py
@@ -18,12 +18,16 @@ from django.urls import path, include
 from rest_framework import routers
 from users import views
 
+admin.site.site_header = "Genemator's API"
+admin.site.site_title = "Genemator's Portal"
+admin.site.index_title = "Superuser Panel :: Genemator's"
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('user/', include(router.urls)),
     path('post/', include('posts.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
